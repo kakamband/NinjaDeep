06dbc67068d8b6fa2715fc7f39ae29e99130ef47
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2020-11-02 18:53:15 +0530

    Ninja

--- ninjadeep/__init__.py
+++ ninjadeep/__init__.py
@@ -1,178 +1,65 @@
-from requests import get
-import pylast
-import asyncio
-from distutils.util import strtobool as sb
-from logging import basicConfig, getLogger, INFO, DEBUG
-import os
-import sys
-from telethon.sessions import StringSession
-from telethon import TelegramClient
-from ninjadeep.ninjadeepConfig import Var
+#    NinjaDeep - UserBot
+#    Copyright (C) 2020 ninjadeep
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import html
 import time
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+from telethon.utils import get_input_location
+from ninjadeep import ALIVE_NAME
+from ninjadeep.__init__ import StartTime
+from datetime import datetime
 
-if Var.STRING_SESSION:
-    session_name = str(Var.STRING_SESSION)
-    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
-else:
-    session_name = "startup"
-    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
+# /start
+started = f"**Welcome To NinjaDeep**\nHi, this is the assistant bot of {ALIVE_NAME}.\nSend `/help` to see what you can do here!\n\n(c) @NinjaDeepSUPPORT"
 
-StartTime = time.time()
-telever = "5.2"
+# /help
+helpmefast = "Here are the things that you can do with this bot!\n\n`/info @username` - get information about the user.\n`/ping` - Ping stats\n`/tr <lang_code>` - Use as reply to the text to translate, language codes can be foung [here](https://t.me/NinjaDeepOT/14)!\n`/id` - To get user/sender id.\n`/logs` - To view the app logs.\n`/usage` - To get app dyno usage.\n`/help` - This menu.\n\n__Set-up your own ninjadeep via @ninjadeepSupport to get such amazing features and more!__"
 
-CMD_LIST = {}
-CMD_HELP = {}
-INT_PLUG = ""
-LOAD_PLUG = {}
-
-# PaperPlaneExtended Support Vars
-ENV = os.environ.get("ENV", False)
-""" PPE initialization. """
+# /ping
 
 
-# Bot Logs setup:
-if bool(ENV):
-    CONSOLE_LOGGER_VERBOSE = sb(
-        os.environ.get(
-            "CONSOLE_LOGGER_VERBOSE",
-            "False"))
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
 
-    if CONSOLE_LOGGER_VERBOSE:
-        basicConfig(
-            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-            level=DEBUG,
-        )
-    else:
-        basicConfig(
-            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-            level=INFO)
-    LOGS = getLogger(__name__)
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
 
-    # Check if the config was edited by using the already used variable.
-    # Basically, its the 'virginity check' for the config file ;)
-    CONFIG_CHECK = os.environ.get(
-        "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
-
-    if CONFIG_CHECK:
-        LOGS.info(
-            "Please remove the line mentioned in the first hashtag from the config.env file"
-        )
-        quit(1)
-
-    # Logging channel/group configuration.
-    BOTLOG_CHATID = os.environ.get("BOTLOG_CHATID", None)
-    try:
-        BOTLOG_CHATID = int(BOTLOG_CHATID)
-    except BaseException:
-        pass
-
-    # Userbot logging feature switch.
-    BOTLOG = sb(os.environ.get("BOTLOG", "False"))
-
-    # Bleep Blop, this is a bot ;)
-    PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
-
-    # Console verbose logging
-    CONSOLE_LOGGER_VERBOSE = sb(
-        os.environ.get(
-            "CONSOLE_LOGGER_VERBOSE",
-            "False"))
-
-    # SQL Database URI
-    DB_URI = os.environ.get("DATABASE_URL", None)
-
-    # OCR API key
-    OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
-
-    # remove.bg API key
-    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
-
-    # For screencapture plugin
-    SCREEN_SHOT_LAYER_ACCESS_KEY = os.environ.get(
-        "SCREEN_SHOT_LAYER_ACCESS_KEY", None)
-
-    # Chrome Driver and Headless Google Chrome Binaries
-    CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
-    GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
-
-    # For bit.ly plugin
-    BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
-
-    # OpenWeatherMap API Key
-    OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
-
-    # Anti Spambot Config
-    ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT", "False"))
-
-    ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
-
-    # Youtube API key
-    YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
-
-    # Default .alive name
-    ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
-
-    # for autopic
-    AUTOPIC_TEXT = os.environ.get(
-        "AUTOPIC_TEXT",
-        "Life Is too Short.\n And so is your TG account.")
-    AUTO_PIC_FONT = os.environ.get("AUTOPIC_FONT", "DejaVuSans.ttf")
-    AUTOPIC_FONT_COLOUR = os.environ.get("AUTOPIC_FONT_COLOUR", None)
-
-    CMD_HNDLR = os.environ.get("CMD_HNDLR", r"\.")
-
-    LESS_SPAMMY = os.environ.get("LESS_SPAMMY", True)
-
-    # Time & Date - Country and Time Zone
-    COUNTRY = str(os.environ.get("COUNTRY", ""))
-
-    TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
-
-    # Clean Welcome
-    CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
-
-    # CUSTOM PMPERMIT
-    CUSTOM_PMPERMIT = os.environ.get("CUSTOM_PMPERMIT", None)
-
-    # PMPERMIT
-    COUNT_MSG = 0
-    COUNT_PM = {}
-
-    # Last.fm Module
-    BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
-    DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
-
-    LASTFM_API = os.environ.get("LASTFM_API", None)
-    LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
-    LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
-    LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
-    LASTFM_PASS = pylast.md5(LASTFM_PASSWORD_PLAIN)
-    if not LASTFM_USERNAME == "None":
-        lastfm = pylast.LastFMNetwork(api_key=LASTFM_API,
-                                      api_secret=LASTFM_SECRET,
-                                      username=LASTFM_USERNAME,
-                                      password_hash=LASTFM_PASS)
-    else:
-        lastfm = None
-
-    # Google Drive Module
-    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
-    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
-    G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
-    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY",
-                                             "./downloads")
-else:
-    # Put your ppe vars here if you are using local hosting
-    PLACEHOLDER = None
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
 
 
-# Global Variables
-COUNT_MSG = 0
-USERS = {}
-COUNT_PM = {}
-LASTMSG = {}
-CMD_HELP = {}
-ISAFK = False
-AFKREASON = None
-# End of PaperPlaneExtended Support Vars
+start = datetime.now()
+end = datetime.now()
+ms = (end - start).microseconds / 1000
+forping = f"🏓Ping speed: {ms}"
