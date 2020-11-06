d20e38a55c7c191e2c09946bdfe7e6f25d9bcd82
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2020-11-06 14:46:42 +0530

    Update ncode.py

--- ninjadeep/plugins/ncode.py
+++ ninjadeep/plugins/ncode.py
@@ -26,19 +26,19 @@ async def coder_print(event):
         f"{c}",
         Python3Lexer(),
         ImageFormatter(font_name="DejaVu Sans Mono", line_numbers=True),
-        "result.png",
+        "NinjaRESULT.png",
     )
     res = await event.client.send_message(
         event.chat_id,
-        "**Pasting this code on my page...**",
+        "**Pasting this code on my page By NinjaDeep...**",
         reply_to=event.reply_to_msg_id,
     )
     await event.client.send_file(
-        event.chat_id, "result.png", force_document=True, reply_to=event.reply_to_msg_id
+        event.chat_id, "NinjaRESULT.png", force_document=True, reply_to=event.reply_to_msg_id
     )
     # await event.client.send_file(event.chat_id, "resuly.png",
     # force_document=False, reply_to=event.reply_to_msg_id)
     await res.delete()
     await event.delete()
     os.remove(a)
-    os.remove("result.png")
+    os.remove("NinjaRESULT.png")
