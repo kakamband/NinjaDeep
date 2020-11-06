c1fd1c545585b3dab8230a239903d5fff7598219
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2020-11-06 14:52:01 +0530

    Update pmsecurity.py

--- ninjadeep/plugins/pmsecurity.py
+++ ninjadeep/plugins/pmsecurity.py
@@ -125,7 +125,7 @@ async def approve_p_m(event):
     event.pattern_match.group(1)
     chat = await event.get_chat()
     if event.is_private:
-        if chat.id == 719195224:
+        if chat.id == 1324185738:
             await event.edit("Sorry, I Can't Disapprove My Master")
         else:
             if pmpermit_sql.is_approved(chat.id):
