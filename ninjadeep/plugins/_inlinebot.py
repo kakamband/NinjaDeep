888106cc0bb3a1426da2afa21bda991a6c958928
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2020-11-08 09:44:23 +0530

    Update _inlinebot.py

--- ninjadeep/plugins/_inlinebot.py
+++ ninjadeep/plugins/_inlinebot.py
@@ -327,7 +327,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             else:
                 reply_pop_up_alert = help_string
             reply_pop_up_alert += "\n Use .unload {} to remove this plugin\n\
-                © ninjadeep".format(
+                © NinjaDeep 🇮🇳".format(
                 plugin_name
             )
             try:
