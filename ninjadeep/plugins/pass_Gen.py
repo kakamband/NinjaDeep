3832f510d6ef7bae15e8aa9f4a52102ba20f7093
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2020-11-08 11:34:38 +0530

    Pass_Gen

---
+++ ninjadeep/plugins/pass_Gen.py
@@ -0,0 +1,46 @@
+print("\033[1;32;40m By----->@DamnitZGuri ☆<----- \n")
+
+
+import asyncio
+from ninjadeep.utils import admin_cmd
+import string
+import random
+
+
+@ninjadeep.on(admin_cmd(pattern=pGen"(.*)", outgoing=True))
+
+async def _(event):
+    if event.fwd_from:
+        return
+        
+await (event, "Processing. . . .")
+
+def Pass_Gen():
+	s1 = string.ascii_uppercase
+	s2 = string.ascii_lowercase
+	s3 = string.digits
+	s4 = string.punctuation
+	
+	service_name = input("Enter Your Site or platform which you want to Generate\n")
+	plen = int(input("Enter Your Pass Length\n"))
+	s = []
+	s.extend(list(s1))
+	s.extend(list(s2))
+	s.extend(list(s3))
+	s.extend(list(s4))
+	
+	await (event, "Your Pass Is Generating. . . .")
+	
+	random.shuffle(s)
+	print("Your Generated Pass Is\n")
+	print("".join(s[0:plen]))
+	
+	file = open("pass.txt" , "a")
+	file.write(f"\n{service_name} pass is ---> ") 
+	file.write(f"".join(s[0:plen]))
+	file.close()
+	
+	print(event,"Password has been saved in the file")
+
+if __name__=="__main__":
+	Pass_Gen()
