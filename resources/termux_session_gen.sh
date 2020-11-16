1120cc0d964339bba00b26cdfce0d1c6f5b3919e
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2020-11-16 11:53:57 +0530

    Session_String

---
+++ resources/termux_session_gen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+echo "
+                          ₦Ї₦ℑ₳
+                _________            ___________    ____________     __________
+                | |                 |       |                      |                         |                   |
+                | |                 |       |                      |                         |                    |
+                | |                 |       |_______          |________           |__________|
+                | |                 |       |                      |                         |
+                __________           ___________    ____________    |
+"
+# Termux session string generator for NinjaDeep
+echo Starting dependency installation in 5 seconds...
+sleep 5
+apt-get update
+apt-get upgrade -y
+pkg upgrade -y
+pkg install python wget -y
+wget https://raw.githubusercontent.com/Lovedeep-ViRk/NinjaDeep/master/resources/ninjadeep-setup.py
+pip install telethon
+python ninjadeep-setup.py
