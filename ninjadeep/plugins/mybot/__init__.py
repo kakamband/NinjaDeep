f0341813f299f41bbe479a2ea2408d328efbd66b
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2020-11-21 07:33:10 +0530

    Update __init__.py

--- ninjadeep/plugins/mybot/__init__.py
+++ ninjadeep/plugins/mybot/__init__.py
@@ -28,8 +28,8 @@ from datetime import datetime
 started = f"**Welcome To NinjaDeep**\nHi, this is the NinjaDeep Assistant bot of {ALIVE_NAME}.\nSend /help to see what you can do here!\n\n(c) @NinjaDeepSUPPORT"
 ),
                     ],
-                    [Button.url("Creator👑", "https://t.me/Lovedeep_ViRk")],
-    Button.url("SUPPORT", "https://t.me/NinjaDeepSUPPORT"),
+                    Button.url("Creator👑", "https://t.me/Lovedeep_ViRk"),
+    [Button.url("SUPPORT", "https://t.me/NinjaDeepSUPPORT")],
 
 # /help
 helpmefast="Here are the things that you can do with this bot!\n\n`/info @username` - get information about the user.\n`/ping` - Ping stats\n`/tr <lang_code>` - Use as reply to the text to translate, language codes can be foung [here](https://t.me/NinjaDeepOT/14)!\n`/id` - To get user/sender id.\n`/logs` - To view the app logs.\n`/usage` - To get app dyno usage.\n`/help` - This menu.\n\n__Set-up your own ninjadeep via @ninjadeepSupport to get such amazing features and more!__"
