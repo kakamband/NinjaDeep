e0e0ebdcbfca0e10403ace33226c02002bd88428
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2020-11-29 21:16:59 +0530

    Update and rename ninjadeepConfig.py to ninjadeepConfigs.py

--- ninjadeep/ninjadeepConfigs.py
+++ ninjadeep/ninjadeepConfigs.py
@@ -20,7 +20,7 @@ from telethon.tl.types import ChatBannedRights
 ENV = bool(os.environ.get("ENV", False))
 
 
-class Var(object):
+class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 6))
     # 6 is a placeholder
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
